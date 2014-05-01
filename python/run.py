f0a1c6aff6012367accd2803a73769b9dab5fe14
Ivan Pedrazas <ipedrazas@gmail.com>
2014-05-01 10:10:24 +0100

    Celery task added

--- python/run.py
+++ python/run.py
@@ -1,5 +1,24 @@
 from eve import Eve
+import pdb
+
+from worker import populate_dotmark
+
+
+
+def after_insert(resource, items):
+	print "after insert"
+	for item in items:
+		populate_dotmark.delay(item)
+
+
+def before_insert(resource_name, items):
+	print "Something is going to be inserted"
+
 app = Eve()
+
+
+app.on_insert += before_insert
+app.on_inserted += after_insert
 
 if __name__ == '__main__':
     app.run( host = '0.0.0.0', port = 5000, debug = True)
---
+++ python/worker.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from celery import Celery
+
+import urllib2
+from BeautifulSoup import BeautifulSoup
+
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client.eve
+
+def make_celery(app):
+    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
+
+flask_app = Flask(__name__)
+flask_app.config.update(
+    CELERY_BROKER_URL='redis://localhost:6379',
+    CELERY_RESULT_BACKEND='redis://localhost:6379'
+)
+celery = make_celery(flask_app)
+
+
+@celery.task()
+def populate_dotmark(item):
+	if 'url' and '_id' in item:
+		print "processing %s" % item['url']
+		soup = BeautifulSoup(urllib2.urlopen(item['url']))
+		title = soup.title.string
+		updates = {'title': title}
+		db.dotmarks.update({'_id': item['_id']}, {"$set": updates}, upsert=False)
+		return 1
