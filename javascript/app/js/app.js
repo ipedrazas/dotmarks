5c41ded21462073d2e1154237005c6aad5985acc
Ivan Pedrazas <ipedrazas@gmail.com>
2014-05-01 18:22:29 +0100

    excluding js unittest

---
+++ javascript/app/js/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('dotApp', ['ngRoute'])
+.config(function ($routeProvider) {
+	  $routeProvider
+		  .when('/dotmarks', {templateUrl: 'partials/dotmarks.html', controller: 'dotMarkController'})
+		  .when('/wn', {templateUrl: 'partials/entries.html', controller: 'wnController'})
+		  .otherwise({redirectTo: '/home'});
+});
---
+++ javascript/app/js/controllers.js
@@ -0,0 +1,90 @@
+'use strict';
+
+function log(entry){
+    if( console && console.log ) {
+            console.log(entry);
+    }
+}
+
+Date.prototype.yyyymmdd = function() {
+   var yyyy = this.getFullYear().toString();
+   var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based
+   var dd  = this.getDate().toString();
+   return yyyy + (mm[1]?mm:"0"+mm[0]) + (dd[1]?dd:"0"+dd[0]); // padding
+  };
+
+/* Controllers */
+
+angular.module('dotApp').controller('dotMarkController', ['$scope', 'api', '$routeParams', function ($scope, api, $routeParams) {
+  	$scope.refreshEntries = function(){
+        api.getDotMarksEntries().success(function (data) {
+        	var elems = new Array()
+        	 _.each(data._items, function(item){
+        	 	// log(item);
+        	 	// item.start = new Date(item.start);        	 	
+        	 	elems.push(item);
+        	 });
+        	 $scope.dotmarks = elems;
+        });
+    };
+    $scope.refreshEntries();
+
+  }]);
+
+
+angular.module('dotApp').controller('wnController', ['$scope', 'wnapi', '$routeParams', function ($scope, api, $routeParams) {
+  	$scope.refreshEntries = function(){
+        api.getWNEntries().success(function (data) {
+        	var elems = new Array()
+        	 _.each(data._items, function(item){
+        	 	item.start = (new Date(item.start)).yyyymmdd();
+
+        	 	if(item.end){
+        	 		item.end = (new Date(item.end)).yyyymmdd();
+        	 	}
+        	 	log(item);      	 	
+        	 	elems.push(item);
+        	 });
+        	 $scope.entries = elems;
+        });
+    };
+    $scope.refreshEntries();
+
+  }]);
+
+
+angular.module('dotApp').factory('api', ['$http', function($http) {
+    var dotmarksUrl = "http://localhost:5000/dotmarks";
+    return {
+        getDotMarksEntries: function() {
+            return $http.get(dotmarksUrl);
+        },
+        saveDotMark: function(entry) {
+            var config = {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                responseType: "application/json",
+            };
+            return $http.post(projectUrl, entry, config);
+        }
+    };
+}]);
+
+angular.module('dotApp').factory('wnapi', ['$http', function($http) {
+    var wnUrl = "http://localhost:5000/entries";
+    return {
+        getWNEntries: function() {
+            return $http.get(wnUrl);
+        },
+        saveDotMark: function(entry) {
+            var config = {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                responseType: "application/json",
+            };
+            return $http.post(projectUrl, entry, config);
+        }
+    };
+}]);
