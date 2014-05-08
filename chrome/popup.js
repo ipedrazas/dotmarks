1afb6f486b4db569b36c14264eee2d39a815d00e
Ivan Pedrazas <ipedrazas@gmail.com>
2014-05-08 19:56:11 +0100

    chrome extension folder

---
+++ chrome/popup.js
@@ -0,0 +1,76 @@
+// This callback function is called when the content script has been 
+// injected and returned its results
+function onPageInfo(o)  { 
+    document.getElementById('title').value = o.title; 
+    document.getElementById('url').value = o.url; 
+    // document.getElementById('summary').innerText = o.summary; 
+} 
+
+// Global reference to the status display SPAN
+var statusDisplay = null;
+
+// POST the data to the server using XMLHttpRequest
+function addBookmark() {
+    // Cancel the form submit
+    event.preventDefault();
+
+    // The URL to POST our data to
+    var postUrl = 'http://localhost:5000/dotmarks';
+
+    // Set up an asynchronous AJAX POST request
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', postUrl, true);
+    
+    // Prepare the data to be POSTed
+    var title = document.getElementById('title').value;
+    var url = document.getElementById('url').value;    
+    var tags = document.getElementById('tags').value;
+    var user = 'ivan';
+    
+    // Set correct header for form data 
+    xhr.setRequestHeader('Content-type', 'application/json');
+
+    // Handle request state change events
+    xhr.onreadystatechange = function() { 
+        // If the request completed
+        if (xhr.readyState == 4) {
+            statusDisplay.innerHTML = '';
+            if (xhr.status == 201) {
+                // If it was a success, close the popup after a short delay
+                statusDisplay.innerHTML = 'Saved!';
+                window.setTimeout(window.close, 1000);
+            } else {// Show what went wrong
+                statusDisplay.innerHTML = 'Error saving: ' + xhr.statusText;
+            }
+        }
+    };
+
+    xhr.send(serializeObject(user, title, url, tags));
+    statusDisplay.innerHTML = 'Saving...';
+}
+
+ function serializeObject(user, title, url, tags){
+    var o = {};
+    o['username'] = user;
+    o['url'] = url;    
+    if(tags !== undefined) {
+        o['tags'] = tags.split(" ");    
+    }
+    if(title !== undefined) {
+        o['title'] = title;
+    }
+    console.log(JSON.stringify(o));
+    
+    return JSON.stringify(o);
+};
+
+// When the popup HTML has loaded
+window.addEventListener('load', function(evt) {
+    // Handle the bookmark form submit event with our addBookmark function
+    document.getElementById('addbookmark').addEventListener('submit', addBookmark);
+    // Cache a reference to the status display SPAN
+    statusDisplay = document.getElementById('status-display');
+    // Call the getPageInfo function in the background page, injecting content_script.js 
+    // into the current HTML page and passing in our onPageInfo function as the callback
+    chrome.extension.getBackgroundPage().getPageInfo(onPageInfo);
+});
