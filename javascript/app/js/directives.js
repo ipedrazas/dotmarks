242a14e0f827cb086d504cd6a7abeea4329add86
Ivan Pedrazas <ipedrazas@gmail.com>
2014-05-26 19:08:39 +0100

    click refined, updates countes inline

--- javascript/app/js/directives.js
+++ javascript/app/js/directives.js
@@ -1,20 +1,13 @@
-angular.module('dotApp').directive('targetUrl2', ['appaudit', function (appaudit) {
-    return function (scope, element, attrs) {
-      element.bind('click', function (event) {
-            var source_id = element.attr('data-origin');
-            appaudit.clickDotMark(source_id);
-      });
-    };
-  }]);
 
 angular.module('dotApp').directive('a', ['appaudit', function (appaudit) {
     return {
         restrict: 'E',
         link: function(scope, elem, attrs) {
-            if(attrs.origin !== undefined && attrs.origin.length >20){
+            if(scope.dotmark !== undefined){
                 elem.on('click', function(e){
-                    log(attrs);
-                    appaudit.clickDotMark(attrs.origin);
+                    log(scope.dotmark);
+                    scope.dotmark.views++;
+                    appaudit.clickDotMark(scope.dotmark._id);
                 });
             }
         }
